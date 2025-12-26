1280e30f538caa6ff137e3ac3696c228130d2255
rchmdndy <rachmadandyms@gmail.com>
2025-12-26 19:02:13 +0700

    fix: Make anthropometry fields non-nullable

--- internal/domain/anthropometry.go
+++ internal/domain/anthropometry.go
@@ -7,14 +7,14 @@ import (
 
 type Anthropometry struct {
 	gorm.Model            // <- ID, CreatedAt, UpdatedAt
-	ChildID    *uuid.UUID `gorm:"type:uuid;unique" json:"child_id"`
+	ChildID    uuid.UUID `gorm:"type:uuid;" json:"child_id"`
 	Child      *Child     `gorm:"foreignKey:ChildID" json:"child"`
-	UserID     *uuid.UUID `gorm:"type:uuid" json:"user_id"`
+	UserID     uuid.UUID `gorm:"type:uuid" json:"user_id"`
 	User       *User      `gorm:"foreignKey:UserID" json:"user"`
 
 	Weight            float32  `gorm:"type:decimal(5, 2); not null" json:"weight"`
 	Height            float32  `gorm:"type:decimal(5, 2); not null" json:"height"`
-	HeadCircumference *float32 `gorm:"decimal(5, 2)" json:"head_circumference"`
+	HeadCircumference float32 `gorm:"decimal(5, 2)" json:"head_circumference"`
 	ZScoreBBU         float32  `gorm:"type:decimal(4, 2)" json:"zscore_bbu"`
 	ZScoreTBU         float32  `gorm:"type:decimal(4, 2)" json:"zscore_tbu"`
 	ZScoreBBTB        float32  `gorm:"type:decimal(4, 2)" json:"zscore_bbtb"`
